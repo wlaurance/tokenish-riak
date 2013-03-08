c4eaae3da88065b09794f717c78e392e492774cf
wlaurance <w.laurance@gmail.com>
2013-03-08 14:27:53 -0500

    Tokenish passing

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-module.exports = {
-  
+var riak = require('riak-js');
+module.exports = function(config){
+  if(!config){
+    config = {
+      bucket:'tokenish'
+    };
+  }
+  var client = riak.getClient({
+    host: config.host || '127.0.0.1',
+    port: config.port || 8098
+  });
+  return {
+    get:function(id, callback){
+      client.get(config.bucket, id, function(err, tokens, meta){
+        if (typeof tokens.length === 'undefined'){
+          tokens = [];
+        }
+        callback(err, tokens);
+      });
+    },
+    add:function(id, token, callback){
+      client.get(config.bucket, id, function(err, tokens, meta){
+        if (typeof tokens.length === 'undefined'){
+          tokens = [];
+        }
+        tokens.push(token);
+        client.save(config.bucket, id, tokens, function(err){
+          callback(err, token);
+        });
+      });
+    }
+  };
 };
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 var tr = require('./index'),
-  assert = require('assert');
+  assert = require('assert'),
+  tr = tr(),
+  token;
 
 describe('tokenish riak', function(){
   it('should have an add method', function(){
@@ -9,13 +11,24 @@ describe('tokenish riak', function(){
     assert.equal(typeof(tr.get), 'function');
   });
   it('should add a token to the list based on a key', function(done){
-    tr.add('id', 'token', function(err){
+    token = new Date().toJSON();
+    tr.add('id', token, function(err){
       done();
     });
   });
   it('should get a list of tokens', function(done){
     tr.get('id', function(err, tokens){
+      assert.equal('number', typeof tokens.length);
       assert.ok(tokens.length);
+      var found = false,
+        i;
+      for (i in tokens){
+        if(tokens[i] === token){
+          found = true;
+          break;
+        }
+      }
+      assert.ok(found);
       done();
     });
   });
